e8295d03dd0c8076453c66eeace2331bdf8f63b9
Alexey <alexeykira@icloud.com>
2022-12-20 16:17:44 +0200

    new repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu4APLCULwXJOBwzua8_BMn0xH3mKwAEKgrA&usqp=CAU' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiozmKVeB0FUnbJ3G7dRCyaoGGOKFZMCmVRBS61upm-a1IqL0xVZ0k97S7zAJxVciAj90&usqp=CAU' />
+            </div>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU/' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
